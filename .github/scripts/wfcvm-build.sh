cac283de4d7dd08dd23bcdc807b0c554d3543817
Mei-Hui Su <mei@MeiPro.local>
2022-03-13 11:37:09 -0700

    add ci

---
+++ .github/scripts/wfcvm-build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+tmp=`uname -s`
+
+if [ $tmp == 'Darwin' ]; then
+##for macOS, make sure have automake/aclocal
+  brew install automake
+fi
+
+aclocal
+automake --add-missing
+autoconf
+cd data
+./make_data_files.py
+cd ..
+./configure --prefix=$UCVM_INSTALL_PATH/model/wfcvm
+make
+make install
+
