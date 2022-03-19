2c521bc00190a41f0f33e2039346f7d40af35184
Mei-Hui Su <mei@MeiPro.local>
2022-03-19 16:42:07 -0700

    reinstall gcc in runner for mac os

--- .github/scripts/wfcvm-build.sh
+++ .github/scripts/wfcvm-build.sh
@@ -5,13 +5,12 @@ tmp=`uname -s`
 if [ $tmp == 'Darwin' ]; then
 ##for macOS, make sure have automake/aclocal
   brew install automake
+  brew reinstall gcc
 fi
 
 aclocal
 automake --add-missing
 autoconf
-cd data
-cd ..
 ./configure --prefix=$UCVM_INSTALL_PATH/model/wfcvm
 make
 make install
